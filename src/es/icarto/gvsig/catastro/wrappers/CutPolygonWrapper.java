5e08b4e51e423b7e541912746fb74896a8f0b7ef
psanxiao <psanxiao@icarto.es>
2011-08-09 13:57:25 -0500

    Created wrapper for the CutPolygonExtension

---
+++ src/es/icarto/gvsig/catastro/wrappers/CutPolygonWrapper.java
@@ -0,0 +1,23 @@
+package es.icarto.gvsig.catastro.wrappers;
+
+import com.iver.cit.gvsig.CutPolygonExtension;
+
+public class CutPolygonWrapper extends CutPolygonExtension {
+	
+	public void initialize() {
+		super.initialize();
+	}
+	
+	public void execute(String s) {
+		super.execute(s);
+	}
+		
+	public boolean isEnabled() {
+		return true;
+	}
+
+	public boolean isVisible() {
+		return true;
+	}
+
+}
