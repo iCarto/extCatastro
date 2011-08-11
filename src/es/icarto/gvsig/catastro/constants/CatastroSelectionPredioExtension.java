a3f836c7444f9f7d56f3b2a43472dcf126ea3b13
Andrés Maneiro <amaneiro@icarto.es>
2011-08-11 10:45:07 -0500

    Add selection menu

---
+++ src/es/icarto/gvsig/catastro/constants/CatastroSelectionPredioExtension.java
@@ -0,0 +1,27 @@
+package es.icarto.gvsig.catastro.constants;
+
+import com.iver.andami.plugins.Extension;
+
+public class CatastroSelectionPredioExtension extends Extension {
+
+    @Override
+    public void initialize() {
+	// TODO Auto-generated method stub
+    }
+
+    @Override
+    public void execute(String actionCommand) {
+	// TODO Auto-generated method stub
+    }
+
+    @Override
+    public boolean isEnabled() {
+	return true;
+    }
+
+    @Override
+    public boolean isVisible() {
+	return true;
+    }
+
+}
