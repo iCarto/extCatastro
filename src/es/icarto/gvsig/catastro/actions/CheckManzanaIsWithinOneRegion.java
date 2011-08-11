d2e21880f433e475e465460f5ebf4d969b866226
psanxiao <psanxiao@icarto.es>
2011-08-11 10:24:34 -0500

    Created CheckManzanaIsWithinOneRegion rule: Not working yet

---
+++ src/es/icarto/gvsig/catastro/actions/CheckManzanaIsWithinOneRegion.java
@@ -0,0 +1,33 @@
+package es.icarto.gvsig.catastro.actions;
+
+import org.gvsig.fmap.core.NewFConverter;
+
+import com.iver.cit.gvsig.fmap.core.IFeature;
+import com.iver.cit.gvsig.fmap.core.IGeometry;
+import com.iver.cit.gvsig.fmap.edition.IRowEdited;
+import com.vividsolutions.jts.geom.Geometry;
+
+public class CheckManzanaIsWithinOneRegion implements ITopologicalRule {
+
+	IRowEdited selectedRow;
+
+	public CheckManzanaIsWithinOneRegion(IRowEdited selectedRow) {
+		this.selectedRow = selectedRow;
+	}
+
+	@Override
+	public boolean isObey() {
+		IGeometry manzanaGeom = ((IFeature) selectedRow.getLinkedRow())
+				.getGeometry();
+		Geometry manzanaJTSGeom = NewFConverter.toJtsGeometry(manzanaGeom);
+		Geometry regionJTSGeom = getRegionGeom();
+		manzanaJTSGeom.within(regionJTSGeom);
+		return true;
+	}
+
+	private Geometry getRegionGeom() {
+		return null;
+
+	}
+
+}
