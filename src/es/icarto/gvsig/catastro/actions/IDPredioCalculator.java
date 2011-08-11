7704da9737b5d6173344fac1daa2f57992ccedbc
Andrés Maneiro <amaneiro@icarto.es>
2011-08-11 15:39:53 -0500

    Get last predioID from an invented list

--- src/es/icarto/gvsig/catastro/actions/IDPredioCalculator.java
+++ src/es/icarto/gvsig/catastro/actions/IDPredioCalculator.java
@@ -1,15 +1,17 @@
 package es.icarto.gvsig.catastro.actions;
 
+import java.util.Arrays;
+
 import com.hardcode.gdbms.driver.exceptions.ReadDriverException;
 import com.hardcode.gdbms.engine.values.Value;
 import com.hardcode.gdbms.engine.values.ValueFactory;
 import com.iver.cit.gvsig.fmap.edition.IRowEdited;
-import com.iver.cit.gvsig.fmap.layers.FLayer;
 import com.iver.cit.gvsig.fmap.layers.FLyrVect;
-import com.iver.cit.gvsig.gui.cad.tools.CutPolygonCADTool;
+
+import es.icarto.gvsig.catastro.utils.Preferences;
 
 public class IDPredioCalculator {
-    
+
     FLyrVect layer = null;
     IRowEdited selectedRow = null;
 
@@ -24,11 +26,37 @@ public class IDPredioCalculator {
 	    numAttr = layer.getRecordset().getFieldCount();
 	    Value[] values = new Value[numAttr];
 	    values = selectedRow.getAttributes().clone();
-	    values[7] = ValueFactory.createValue("5432");
+	    int predioIDIndex = getPredioIDIndex();
+	    values[predioIDIndex] = getNewPredioID();
 	    return values;
 	} catch (ReadDriverException e) {
 	    e.printStackTrace();
 	    return null;
+	}
+    }
+
+    private Value getNewPredioID() {
+	//TODO: get a set of predios taking into account the constants
+	String[] prediosID = {"1", "3", "2"};
+	Arrays.sort(prediosID);
+	return ValueFactory.createValue(prediosID[prediosID.length-1]);
+    }
+
+    private int getPredioIDIndex(){
+	String[] fieldNames;
+	int predioIndex = -1;
+	try {
+	    fieldNames = layer.getRecordset().getFieldNames();
+	    for (int i=0; i<fieldNames.length; i++){
+		if (fieldNames[i].equalsIgnoreCase(Preferences.PREDIO_NAME_IN_DB)){
+		    predioIndex = i;
+		    return predioIndex;
+		}
+	    }
+	    return predioIndex;
+	} catch (ReadDriverException e) {
+	    e.printStackTrace();
+	    return predioIndex;
 	}
     }
 
