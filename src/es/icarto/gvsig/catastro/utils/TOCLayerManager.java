80a01a87725a13496961786cb7107c8a917e6c18
Andrés Maneiro <amaneiro@icarto.es>
2011-08-11 10:44:29 -0500

    Fix typo in variable

--- src/es/icarto/gvsig/catastro/utils/TOCLayerManager.java
+++ src/es/icarto/gvsig/catastro/utils/TOCLayerManager.java
@@ -8,72 +8,72 @@ import com.iver.cit.gvsig.project.documents.view.gui.BaseView;
 
 public class TOCLayerManager {
 
-	private final FLayers layersInTOC;
+    private final FLayers layersInTOC;
 
-	public TOCLayerManager() {
-		BaseView view = (BaseView) PluginServices.getMDIManager()
-				.getActiveWindow();
-		MapControl mapControl = view.getMapControl();
-		layersInTOC = mapControl.getMapContext().getLayers();
-	}
+    public TOCLayerManager() {
+	BaseView view = (BaseView) PluginServices.getMDIManager()
+		.getActiveWindow();
+	MapControl mapControl = view.getMapControl();
+	layersInTOC = mapControl.getMapContext().getLayers();
+    }
 
-	public void setActiveAndVisibleLayersForManzanas() {
-		layersInTOC.setAllActives(false);
-		layersInTOC.setAllVisibles(false);
-		for (int i = 0; i < layersInTOC.getLayersCount(); i++) {
-			String layerName = layersInTOC.getLayer(i).getName();
-			if (layerName.equalsIgnoreCase(Preferences.PREDIOS_LAYER_NAME)) {
-				layersInTOC.getLayer(i).setVisible(true);
-			} else if (layerName
-					.equalsIgnoreCase(Preferences.MANZANAS_LAYER_NAME)) {
-				layersInTOC.getLayer(i).setVisible(true);
-				layersInTOC.getLayer(i).setActive(true);
-			}
-		}
+    public void setActiveAndVisibleLayersForManzanas() {
+	layersInTOC.setAllActives(false);
+	layersInTOC.setAllVisibles(false);
+	for (int i = 0; i < layersInTOC.getLayersCount(); i++) {
+	    String layerName = layersInTOC.getLayer(i).getName();
+	    if (layerName.equalsIgnoreCase(Preferences.PREDIOS_LAYER_NAME)) {
+		layersInTOC.getLayer(i).setVisible(true);
+	    } else if (layerName
+		    .equalsIgnoreCase(Preferences.MANZANAS_LAYER_NAME)) {
+		layersInTOC.getLayer(i).setVisible(true);
+		layersInTOC.getLayer(i).setActive(true);
+	    }
 	}
+    }
 
-	public void setActiveAndVisibleLayersForPredios() {
-		layersInTOC.setAllActives(false);
-		layersInTOC.setAllVisibles(false);
-		for (int i = 0; i < layersInTOC.getLayersCount(); i++) {
-			String layerName = layersInTOC.getLayer(i).getName();
-			if (layerName
-					.equalsIgnoreCase(Preferences.CONSTRUCCIONES_LAYER_NAME)) {
-				layersInTOC.getLayer(i).setVisible(true);
-			} else if (layerName
-					.equalsIgnoreCase(Preferences.PREDIOS_LAYER_NAME)) {
-				layersInTOC.getLayer(i).setVisible(true);
-				layersInTOC.getLayer(i).setActive(true);
-			}
-		}
+    public void setActiveAndVisibleLayersForPredios() {
+	layersInTOC.setAllActives(false);
+	layersInTOC.setAllVisibles(false);
+	for (int i = 0; i < layersInTOC.getLayersCount(); i++) {
+	    String layerName = layersInTOC.getLayer(i).getName();
+	    if (layerName
+		    .equalsIgnoreCase(Preferences.CONSTRUCCIONES_LAYER_NAME)) {
+		layersInTOC.getLayer(i).setVisible(true);
+	    } else if (layerName
+		    .equalsIgnoreCase(Preferences.PREDIOS_LAYER_NAME)) {
+		layersInTOC.getLayer(i).setVisible(true);
+		layersInTOC.getLayer(i).setActive(true);
+	    }
 	}
+    }
 
-	public void setActiveAndVisibleLayersForConstrucciones() {
-		layersInTOC.setAllActives(false);
-		layersInTOC.setAllVisibles(false);
-		for (int i = 0; i < layersInTOC.getLayersCount(); i++) {
-			String layerName = layersInTOC.getLayer(i).getName();
-			if (layerName.equalsIgnoreCase(Preferences.PREDIOS_LAYER_NAME)) {
-				layersInTOC.getLayer(i).setVisible(true);
-			} else if (layerName
-					.equalsIgnoreCase(Preferences.CONSTRUCCIONES_LAYER_NAME)) {
-				layersInTOC.getLayer(i).setVisible(true);
-				layersInTOC.getLayer(i).setActive(true);
-			}
-		}
+    public void setActiveAndVisibleLayersForConstrucciones() {
+	layersInTOC.setAllActives(false);
+	layersInTOC.setAllVisibles(false);
+	for (int i = 0; i < layersInTOC.getLayersCount(); i++) {
+	    String layerName = layersInTOC.getLayer(i).getName();
+	    if (layerName.equalsIgnoreCase(Preferences.PREDIOS_LAYER_NAME)) {
+		layersInTOC.getLayer(i).setVisible(true);
+	    } else if (layerName
+		    .equalsIgnoreCase(Preferences.CONSTRUCCIONES_LAYER_NAME)) {
+		layersInTOC.getLayer(i).setVisible(true);
+		layersInTOC.getLayer(i).setActive(true);
+	    }
 	}
+    }
 
-	public void setVisibleAllLayers() {
-		layersInTOC.setAllVisibles(true);
-	}
+    public void setVisibleAllLayers() {
+	layersInTOC.setAllVisibles(true);
+    }
 
-	public FLyrVect getLayerManzana() {
-		for (int i = 0; i < layersInTOC.getLayersCount(); i++) {
-			if (layersInTOC.getLayer(i).getName().equalsIgnoreCase(
-					Preferences.MANZANAS_LAYER_NAME)) {
-				return (FLyrVect) layersInTOC.getLayer(i);
-			}
-		}
-		return null;
+    public FLyrVect getLayerManzana() {
+	for (int i = 0; i < layersInTOC.getLayersCount(); i++) {
+	    if (layersInTOC.getLayer(i).getName().equalsIgnoreCase(
+		    Preferences.MANZANAS_LAYER_NAME)) {
+		return (FLyrVect) layersInTOC.getLayer(i);
+	    }
 	}
+	return null;
+    }
 }
