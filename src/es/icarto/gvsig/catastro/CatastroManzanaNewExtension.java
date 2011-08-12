4f73a39ba8a1c7c60269e3b051c549c1c01f2d65
psanxiao <psanxiao@icarto.es>
2011-08-12 13:12:29 -0500

    Minor fix because of the integration of last commit

--- src/es/icarto/gvsig/catastro/CatastroManzanaNewExtension.java
+++ src/es/icarto/gvsig/catastro/CatastroManzanaNewExtension.java
@@ -4,38 +4,14 @@ import javax.swing.JOptionPane;
 
 import com.iver.andami.PluginServices;
 import com.iver.andami.plugins.Extension;
+
 import es.icarto.gvsig.catastro.constants.ConstantManager;
 import es.icarto.gvsig.catastro.utils.TOCLayerManager;
 import es.icarto.gvsig.catastro.wrappers.InsertAreaWrapper;
 
 public class CatastroManzanaNewExtension extends Extension {
 
-<<<<<<< HEAD
-    private InsertAreaWrapper insertAreaWrapper;
-    private ConstantManager constantManager;
-    private TOCLayerManager tocLayerManager;
-
-    @Override
-    public void initialize() {
-	insertAreaWrapper = new InsertAreaWrapper();
-	insertAreaWrapper.initialize();
-	constantManager = new ConstantManager();
-    }
-
-    @Override
-    public void execute(String actionCommand) {
-	tocLayerManager = new TOCLayerManager();
-	if(constantManager.areConstantsSetForManzana()){
-	    tocLayerManager.setActiveAndVisibleLayersForManzanas();
-	    insertAreaWrapper.execute(actionCommand);
-	} else{
-	    Object[] options = { "OK" };
-	    JOptionPane.showOptionDialog(null, PluginServices.getText(this, "select_region"), "Warning",
-		    JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE,
-		    null, options, options[0]);
-=======
 	private InsertAreaWrapper insertAreaWrapper;
-	private FLayer layer;
 	private ConstantManager constantManager;
 	private TOCLayerManager tocLayerManager;
 
@@ -59,7 +35,6 @@ public class CatastroManzanaNewExtension extends Extension {
 					"select_region"), "Warning", JOptionPane.DEFAULT_OPTION,
 					JOptionPane.WARNING_MESSAGE, null, options, options[0]);
 		}
->>>>>>> Added CheckManzanaIsWithinOneRegion rule with tolerance buffer
 	}
 
 	@Override
