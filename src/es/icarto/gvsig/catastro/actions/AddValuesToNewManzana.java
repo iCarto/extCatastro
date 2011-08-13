538b8abc23270e2e5f926cd466693c72590743e0
psanxiao <psanxiao@icarto.es>
2011-08-13 11:52:48 -0500

    Added new action class AddValuesToNewManzana

---
+++ src/es/icarto/gvsig/catastro/actions/AddValuesToNewManzana.java
@@ -0,0 +1,68 @@
+package es.icarto.gvsig.catastro.actions;
+
+import com.hardcode.gdbms.driver.exceptions.ReadDriverException;
+import com.iver.cit.gvsig.fmap.layers.FLyrVect;
+import com.iver.cit.gvsig.fmap.layers.SelectableDataSource;
+
+import es.icarto.gvsig.catastro.constants.ConstantManager;
+import es.icarto.gvsig.catastro.constants.Constants;
+import es.icarto.gvsig.catastro.utils.Preferences;
+import es.icarto.gvsig.catastro.utils.ToggleEditing;
+
+public class AddValuesToNewManzana {
+
+    int rowIndex;
+    FLyrVect layer = null;
+    SelectableDataSource recordset;
+
+    public AddValuesToNewManzana(FLyrVect layer, int rowIndex) {
+	this.layer = layer;
+	this.rowIndex = rowIndex;
+	try {
+	    recordset = layer.getRecordset();
+	} catch (ReadDriverException e) {
+	    e.printStackTrace();
+	}
+    }
+
+    public void addValues() {
+	try {
+	    int[] updatePositions = new int[recordset.getFieldCount()];
+	    String[] updateValues = new String[recordset.getFieldCount()];
+
+	    updatePositions[0] = getEstadoIndex();
+	    updatePositions[1] = getMunicipioIndex();
+	    updatePositions[2] = getLimiteIndex();
+	    updatePositions[3] = getRegionIndex();
+
+	    ConstantManager constantManager = new ConstantManager();
+	    Constants constants = constantManager.getConstants();
+
+	    updateValues[0] = Preferences.ESTADO;
+	    updateValues[1] = Preferences.MUNICIPIO;
+	    updateValues[2] = Preferences.LIMITE;
+	    updateValues[3] = constants.getRegion();
+
+	    ToggleEditing te = new ToggleEditing();
+	    te.modifyValues(layer, rowIndex, updatePositions, updateValues);
+	} catch (ReadDriverException e) {
+	    e.printStackTrace();
+	}
+    }
+
+    private int getEstadoIndex() throws ReadDriverException {
+	return recordset.getFieldIndexByName(Preferences.ESTADO_NAME_IN_DB);
+    }
+
+    private int getMunicipioIndex() throws ReadDriverException {
+	return recordset.getFieldIndexByName(Preferences.MUNICIPIO_NAME_IN_DB);
+    }
+
+    private int getLimiteIndex() throws ReadDriverException {
+	return recordset.getFieldIndexByName(Preferences.LIMITE_NAME_IN_DB);
+    }
+
+    private int getRegionIndex() throws ReadDriverException {
+	return recordset.getFieldIndexByName(Preferences.REGION_NAME_IN_DB);
+    }
+}
--- src/es/icarto/gvsig/catastro/utils/Preferences.java
+++ src/es/icarto/gvsig/catastro/utils/Preferences.java
@@ -7,7 +7,19 @@ public class Preferences {
     public static final String CONSTRUCCIONES_LAYER_NAME = "construcciones";
     public static final String REGIONES_LAYER_NAME = "regiones";
 
+    public static final String ESTADO_NAME_IN_DB = "EDO_CVE";
+    public static final String MUNICIPIO_NAME_IN_DB = "MUN_CVE";
+    public static final String LIMITE_NAME_IN_DB = "LIM_CVE";
     public static final String PREDIO_NAME_IN_DB = "PRE_CVE";
     public static final String REGION_NAME_IN_DB = "REG_CVE";
     public static final String MANZANA_NAME_IN_DB = "MAN_CVE";
+
+    /*
+     * Those constants should be set when the program starts and they must be
+     * taken from the database taking into account database connection
+     * information. For now they are been "hardcode" here.
+     */
+    public static final String ESTADO = "29";
+    public static final String MUNICIPIO = "010";
+    public static final String LIMITE = "0001";
 }
