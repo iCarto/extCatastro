7ffde117cb1f8093d6a9154b04d6e535f50bb164
psanxiao <psanxiao@icarto.es>
2011-08-15 10:17:05 -0500

    Fixed constants satutsbar behaviour

--- src/es/icarto/gvsig/catastro/CatastroSelectionRegionExtension.java
+++ src/es/icarto/gvsig/catastro/CatastroSelectionRegionExtension.java
@@ -6,43 +6,47 @@ import com.iver.cit.gvsig.fmap.MapControl;
 import com.iver.cit.gvsig.fmap.tools.Behavior.PointBehavior;
 import com.iver.cit.gvsig.project.documents.view.gui.View;
 
+import es.icarto.gvsig.catastro.constants.ConstantManager;
 import es.icarto.gvsig.catastro.constants.ConstantsSelectionListener;
 import es.icarto.gvsig.catastro.utils.Preferences;
 import es.icarto.gvsig.catastro.utils.TOCLayerManager;
 
 public class CatastroSelectionRegionExtension extends Extension {
 
-	TOCLayerManager tocLayerManager;
+    TOCLayerManager tocLayerManager;
 
-	@Override
-	public void initialize() {
-		// TODO Auto-generated method stub
-	}
+    @Override
+    public void initialize() {
+	// TODO Auto-generated method stub
+    }
 
-	@Override
-	public void execute(String actionCommand) {
-		tocLayerManager = new TOCLayerManager();
-		tocLayerManager
-				.setActiveAndVisibleLayer(Preferences.REGIONES_LAYER_NAME);
-		View view = (View) PluginServices.getMDIManager().getActiveWindow();
-		MapControl mc = view.getMapControl();
-		if (!mc.getNamesMapTools().containsKey("constantsSelectionRegion")) {
-			ConstantsSelectionListener csl = new ConstantsSelectionListener(mc);
-			mc.addMapTool("constantsSelectionRegion", new PointBehavior(csl));
-		}
-		mc.setTool("constantsSelectionRegion");
+    @Override
+    public void execute(String actionCommand) {
+	tocLayerManager = new TOCLayerManager();
+	tocLayerManager
+		.setActiveAndVisibleLayer(Preferences.REGIONES_LAYER_NAME);
+	View view = (View) PluginServices.getMDIManager().getActiveWindow();
+	MapControl mc = view.getMapControl();
+	if (!mc.getNamesMapTools().containsKey("constantsSelectionRegion")) {
+	    ConstantsSelectionListener csl = new ConstantsSelectionListener(mc);
+	    mc.addMapTool("constantsSelectionRegion", new PointBehavior(csl));
 	}
+	mc.setTool("constantsSelectionRegion");
+    }
 
-	@Override
-	public boolean isEnabled() {
-		PluginServices.getMainFrame().getStatusBar().setMessage("constants",
-				"R:- M:- P:-");
-		return true;
-	}
+    @Override
+    public boolean isEnabled() {
+	return true;
+    }
 
-	@Override
-	public boolean isVisible() {
-		return true;
+    @Override
+    public boolean isVisible() {
+	ConstantManager constantManager = new ConstantManager();
+	if (constantManager.getConstants() == null) {
+	    PluginServices.getMainFrame().getStatusBar().setMessage(
+		    "constants", "R:- M:- P:-");
 	}
+	return true;
+    }
 
 }
--- src/es/icarto/gvsig/catastro/constants/ConstantsSelectionListener.java
+++ src/es/icarto/gvsig/catastro/constants/ConstantsSelectionListener.java
@@ -23,134 +23,133 @@ import es.icarto.gvsig.catastro.utils.TOCLayerManager;
 
 public class ConstantsSelectionListener extends PointSelectionListener {
 
-	private static final int LAYER_IS_MANZANA = 0;
-	private static final int LAYER_IS_PREDIO = 1;
-	private static final int LAYER_IS_REGION = 2;
+    private static final int LAYER_IS_MANZANA = 0;
+    private static final int LAYER_IS_PREDIO = 1;
+    private static final int LAYER_IS_REGION = 2;
 
-	TOCLayerManager tocLayerManager;
-	private final ConstantManager constantManager;
-	private final Constants constants;
-	private final HashMap<String, Integer> layerCodes;
+    TOCLayerManager tocLayerManager;
+    private final ConstantManager constantManager;
+    private final Constants constants;
+    private final HashMap<String, Integer> layerCodes;
 
-	private JLabel constantsLabel;
+    private JLabel constantsLabel;
 
-	public ConstantsSelectionListener(MapControl mc) {
-		super(mc);
-		tocLayerManager = new TOCLayerManager();
-		constantManager = new ConstantManager();
-		constants = new Constants();
-		layerCodes = new HashMap<String, Integer>();
-		initLayerCodes();
-	}
+    public ConstantsSelectionListener(MapControl mc) {
+	super(mc);
+	tocLayerManager = new TOCLayerManager();
+	constantManager = new ConstantManager();
+	constants = new Constants();
+	layerCodes = new HashMap<String, Integer>();
+	initLayerCodes();
+    }
 
-	private void initLayerCodes() {
-		layerCodes.put(Preferences.MANZANAS_LAYER_NAME, LAYER_IS_MANZANA);
-		layerCodes.put(Preferences.PREDIOS_LAYER_NAME, LAYER_IS_PREDIO);
-		layerCodes.put(Preferences.REGIONES_LAYER_NAME, LAYER_IS_REGION);
-	}
+    private void initLayerCodes() {
+	layerCodes.put(Preferences.MANZANAS_LAYER_NAME, LAYER_IS_MANZANA);
+	layerCodes.put(Preferences.PREDIOS_LAYER_NAME, LAYER_IS_PREDIO);
+	layerCodes.put(Preferences.REGIONES_LAYER_NAME, LAYER_IS_REGION);
+    }
 
-	@Override
-	public void point(PointEvent event) throws BehaviorException {
-		super.point(event);
-		FLyrVect layer = tocLayerManager.getActiveLayer();
-		String layerName = layer.getName();
+    @Override
+    public void point(PointEvent event) throws BehaviorException {
+	super.point(event);
+	FLyrVect layer = tocLayerManager.getActiveLayer();
+	String layerName = layer.getName();
 
-		try {
-			FBitSet selectionIndex;
-			selectionIndex = layer.getRecordset().getSelection();
-			int indexOfFeatureSelected = selectionIndex.nextSetBit(0);
-			if (indexOfFeatureSelected != -1) {
-				Value[] values = layer.getRecordset().getRow(
-						indexOfFeatureSelected);
-				String[] fieldNames = layer.getRecordset().getFieldNames();
-				int predioIndex = -1;
-				int manzanaIndex = -1;
-				int regionIndex = -1;
-				for (int i = 0; i < fieldNames.length; i++) {
-					if (fieldNames[i]
-							.equalsIgnoreCase(Preferences.MANZANA_NAME_IN_DB)) {
-						manzanaIndex = i;
-					} else if (fieldNames[i]
-							.equalsIgnoreCase(Preferences.REGION_NAME_IN_DB)) {
-						regionIndex = i;
-					} else if (fieldNames[i]
-							.equalsIgnoreCase(Preferences.PREDIO_NAME_IN_DB)) {
-						predioIndex = i;
-					}
-				}
-				switch (layerCodes.get(layerName)) {
-				case LAYER_IS_PREDIO:
-					constants.setManzana(values[manzanaIndex].toString());
-					constants.setPredio(values[predioIndex].toString());
-					constants.setRegion(values[regionIndex].toString());
-					break;
-				case LAYER_IS_MANZANA:
-					constants.setManzana(values[manzanaIndex].toString());
-					constants.setRegion(values[regionIndex].toString());
-					break;
-				case LAYER_IS_REGION:
-					constants.setRegion(values[regionIndex].toString());
-					break;
-				}
-				if (constants != null) {
-					int option = JOptionPane.showConfirmDialog(null,
-							PluginServices.getText(this, "selection_confirm"),
-							"Confirm selection", JOptionPane.OK_CANCEL_OPTION,
-							JOptionPane.OK_CANCEL_OPTION, null);
-					if (option == JOptionPane.OK_OPTION) {
-						constantManager.setConstants(constants);
-						updateConstantsStatusBar();
-						tocLayerManager.setVisibleAllLayers();
-						IGeometry geom = layer.getSource().getFeature(
-								indexOfFeatureSelected).getGeometry();
-						layer.getMapContext().getViewPort().setExtent(
-								geom.getBounds2D());
-					} else {
-						constants.clear();
-						updateConstantsStatusBar();
-						layer.getRecordset().clearSelection();
-					}
-				} else {
-					Object[] options = { "OK" };
-					JOptionPane.showOptionDialog(null, PluginServices.getText(
-							this, "selection_is_none"), "Warning",
-							JOptionPane.DEFAULT_OPTION,
-							JOptionPane.WARNING_MESSAGE, null, options,
-							options[0]);
-				}
-			}
-		} catch (ReadDriverException e) {
-			e.printStackTrace();
+	try {
+	    FBitSet selectionIndex;
+	    selectionIndex = layer.getRecordset().getSelection();
+	    int indexOfFeatureSelected = selectionIndex.nextSetBit(0);
+	    if (indexOfFeatureSelected != -1) {
+		Value[] values = layer.getRecordset().getRow(
+			indexOfFeatureSelected);
+		String[] fieldNames = layer.getRecordset().getFieldNames();
+		int predioIndex = -1;
+		int manzanaIndex = -1;
+		int regionIndex = -1;
+		for (int i = 0; i < fieldNames.length; i++) {
+		    if (fieldNames[i]
+			    .equalsIgnoreCase(Preferences.MANZANA_NAME_IN_DB)) {
+			manzanaIndex = i;
+		    } else if (fieldNames[i]
+			    .equalsIgnoreCase(Preferences.REGION_NAME_IN_DB)) {
+			regionIndex = i;
+		    } else if (fieldNames[i]
+			    .equalsIgnoreCase(Preferences.PREDIO_NAME_IN_DB)) {
+			predioIndex = i;
+		    }
 		}
-	}
-
-	private void updateConstantsStatusBar() {
-		addConstantsToStatusBar();
-	}
-
-	private void addConstantsToStatusBar() {
-		MDIFrame mF = (MDIFrame) PluginServices.getMainFrame();
-		NewStatusBar footerStatusBar = mF.getStatusBar();
-
-		Constants constants = constantManager.getConstants();
+		switch (layerCodes.get(layerName)) {
+		case LAYER_IS_PREDIO:
+		    constants.setManzana(values[manzanaIndex].toString());
+		    constants.setPredio(values[predioIndex].toString());
+		    constants.setRegion(values[regionIndex].toString());
+		    break;
+		case LAYER_IS_MANZANA:
+		    constants.setManzana(values[manzanaIndex].toString());
+		    constants.setRegion(values[regionIndex].toString());
+		    break;
+		case LAYER_IS_REGION:
+		    constants.setRegion(values[regionIndex].toString());
+		    break;
+		}
 		if (constants != null) {
-			String constantsInfo = getConstantsInfo(constants);
-			footerStatusBar.setMessage("constants", constantsInfo);
+		    int option = JOptionPane.showConfirmDialog(null,
+			    PluginServices.getText(this, "selection_confirm"),
+			    "Confirm selection", JOptionPane.OK_CANCEL_OPTION,
+			    JOptionPane.OK_CANCEL_OPTION, null);
+		    if (option == JOptionPane.OK_OPTION) {
+			constantManager.setConstants(constants);
+			updateConstantsStatusBar();
+			tocLayerManager.setVisibleAllLayers();
+			IGeometry geom = layer.getSource().getFeature(
+				indexOfFeatureSelected).getGeometry();
+			layer.getMapContext().getViewPort().setExtent(
+				geom.getBounds2D());
+		    } else {
+			constants.clear();
+			layer.getRecordset().clearSelection();
+		    }
+		} else {
+		    Object[] options = { "OK" };
+		    JOptionPane.showOptionDialog(null, PluginServices.getText(
+			    this, "selection_is_none"), "Warning",
+			    JOptionPane.DEFAULT_OPTION,
+			    JOptionPane.WARNING_MESSAGE, null, options,
+			    options[0]);
 		}
+	    }
+	} catch (ReadDriverException e) {
+	    e.printStackTrace();
 	}
+    }
 
-	private String getConstantsInfo(Constants constants) {
-		String region = nullToString(constants.getRegion());
-		String manzana = nullToString(constants.getManzana());
-		String predio = nullToString(constants.getPredio());
+    private void updateConstantsStatusBar() {
+	addConstantsToStatusBar();
+    }
 
-		return "R: " + region + " M: " + manzana + " P: " + predio;
+    private void addConstantsToStatusBar() {
+	MDIFrame mF = (MDIFrame) PluginServices.getMainFrame();
+	NewStatusBar footerStatusBar = mF.getStatusBar();
+
+	Constants constants = constantManager.getConstants();
+	if (constants != null) {
+	    String constantsInfo = getConstantsInfo(constants);
+	    footerStatusBar.setMessage("constants", constantsInfo);
 	}
+    }
 
-	private String nullToString(String s) {
-		if (s == null)
-			return "-";
-		else
-			return s;
-	}
+    private String getConstantsInfo(Constants constants) {
+	String region = nullToString(constants.getRegion());
+	String manzana = nullToString(constants.getManzana());
+	String predio = nullToString(constants.getPredio());
+
+	return "R: " + region + " M: " + manzana + " P: " + predio;
+    }
+
+    private String nullToString(String s) {
+	if (s == null)
+	    return "-";
+	else
+	    return s;
+    }
 }
