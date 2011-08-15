2f773ca3f0745e945a1315a5d6ae4bc1f694f120
psanxiao <psanxiao@icarto.es>
2011-08-15 16:33:04 -0500

    Added extension to clean all constants

---
+++ src/es/icarto/gvsig/catastro/CleanAllConstantsExtension.java
@@ -0,0 +1,34 @@
+package es.icarto.gvsig.catastro;
+
+import com.iver.andami.PluginServices;
+import com.iver.andami.plugins.Extension;
+
+import es.icarto.gvsig.catastro.constants.ConstantManager;
+import es.icarto.gvsig.catastro.constants.Constants;
+
+public class CleanAllConstantsExtension extends Extension {
+
+    @Override
+    public void execute(String actionCommand) {
+	ConstantManager constantManager = new ConstantManager();
+	Constants constants = constantManager.getConstants();
+	constants.clear();
+	PluginServices.getMainFrame().getStatusBar().setMessage("constants",
+		"R:- M:- P:-");
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public boolean isEnabled() {
+	return true;
+    }
+
+    @Override
+    public boolean isVisible() {
+	return true;
+    }
+
+}
