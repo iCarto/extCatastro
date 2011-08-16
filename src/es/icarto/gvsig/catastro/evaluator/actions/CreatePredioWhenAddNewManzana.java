520d809d4cb513e04b6edde50a8220e52f257a59
psanxiao <psanxiao@icarto.es>
2011-08-16 10:07:42 -0500

    Changed the way of creating predio when the user add a new manzana

--- src/es/icarto/gvsig/catastro/evaluator/actions/CreatePredioWhenAddNewManzana.java
+++ src/es/icarto/gvsig/catastro/evaluator/actions/CreatePredioWhenAddNewManzana.java
@@ -1,10 +1,13 @@
 package es.icarto.gvsig.catastro.evaluator.actions;
 
 import com.hardcode.gdbms.driver.exceptions.ReadDriverException;
+import com.hardcode.gdbms.engine.values.Value;
+import com.hardcode.gdbms.engine.values.ValueFactory;
 import com.iver.andami.PluginServices;
 import com.iver.cit.gvsig.fmap.core.IFeature;
 import com.iver.cit.gvsig.fmap.layers.FBitSet;
 import com.iver.cit.gvsig.fmap.layers.FLyrVect;
+
 import es.icarto.gvsig.catastro.utils.Preferences;
 import es.icarto.gvsig.catastro.utils.TOCLayerManager;
 import es.icarto.gvsig.catastro.utils.ToggleEditing;
@@ -26,21 +29,24 @@ public class CreatePredioWhenAddNewManzana implements IAction {
 	    ToggleEditing te = new ToggleEditing();
 	    boolean wasEditingManzanas = false;
 	    if (tocLayerManager.isManzanaLayerInEdition()) {
-		//TODO: save layer
-		//te.stopEditing(sourceLayer, false);
+		// TODO: save layer
+		// te.stopEditing(sourceLayer, false);
 		sourceLayer.setActive(false);
 		wasEditingManzanas = true;
 	    }
 	    destinationLayer.setActive(true);
 	    te.startEditing(destinationLayer);
 	    FBitSet indexes = sourceLayer.getRecordset().getSelection();
-	    IFeature feature = sourceLayer.getSource().getFeature(indexes.nextSetBit(0));
-	    te.addGeometryWithParametrizedValues(feature.getGeometry(), feature.getAttributes().clone(), "_create_new_predio");
-	    //TODO: save layer
-	    //te.stopEditing(destinationLayer, false);
+	    IFeature feature = sourceLayer.getSource().getFeature(
+		    indexes.nextSetBit(0));
+	    te.addGeometryWithParametrizedValues(feature.getGeometry()
+		    .cloneGeometry(), getNewPredioValues(feature),
+		    "_create_new_predio");
+	    // TODO: save layer
+	    // te.stopEditing(destinationLayer, false);
 	    destinationLayer.setActive(false);
 	    if (wasEditingManzanas) {
-		//te.startEditing(sourceLayer);
+		// te.startEditing(sourceLayer);
 		sourceLayer.setActive(true);
 	    }
 	    return true;
@@ -51,6 +57,35 @@ public class CreatePredioWhenAddNewManzana implements IAction {
 	    e.printStackTrace();
 	    return false;
 	}
+    }
+
+    private Value[] getNewPredioValues(IFeature feature) {
+	Value[] predioValues = new Value[11];
+	Value[] featureValues = feature.getAttributes().clone();
+	// int destinationNumFields;
+	// int sourceNumFields;
+	// try {
+	// sourceNumFields = sourceLayer.getRecordset().getFieldCount();
+	// destinationNumFields =
+	// destinationLayer.getRecordset().getFieldCount();
+	// Value[] prediovalues = new Value[destinationNumFields];
+	// for(int i = 0; i < destinationNumFields; i++) {
+	//		
+	// }
+	for (int i = 0; i < featureValues.length; i++) {
+	    predioValues[0] = featureValues[0]; // pais_cve
+	    predioValues[1] = featureValues[1]; // edo_cve
+	    predioValues[2] = featureValues[2]; // mun_cve
+	    predioValues[3] = featureValues[3]; // reg_cve
+	    predioValues[4] = featureValues[4]; // lim_cve
+	    predioValues[5] = featureValues[6]; // man_cve
+	    predioValues[6] = ValueFactory.createNullValue(); // catmetadato_id
+	    predioValues[7] = ValueFactory.createValue(1); // pre_cve
+	    predioValues[8] = featureValues[7]; // pre_area
+	    predioValues[9] = ValueFactory.createNullValue(); // zon_id
+	    predioValues[10] = ValueFactory.createNullValue(); // gid
+	}
+	return predioValues;
     }
 
     private FLyrVect getDestinationLayer() {
