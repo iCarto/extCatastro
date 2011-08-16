7cbbe70ac5ae28ea49afada5b5b506cf74a0b378
Andrés Maneiro <amaneiro@icarto.es>
2011-08-16 13:34:02 -0500

    Disabled construcciones until having the proper values inserted in the new

--- src/es/icarto/gvsig/catastro/CatastroConstruccionNewExtension.java
+++ src/es/icarto/gvsig/catastro/CatastroConstruccionNewExtension.java
@@ -39,7 +39,8 @@ public class CatastroConstruccionNewExtension extends Extension {
 
     @Override
     public boolean isEnabled() {
-	return insertAreaWrapper.isEnabled();
+	return false;
+	//return insertAreaWrapper.isEnabled();
     }
 
     @Override
