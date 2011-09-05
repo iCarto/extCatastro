dc58104eafdedfec91b83ce5cc8973e531a16b2e
Andrés Maneiro <amaneiro@icarto.es>
2011-09-05 19:54:38 +0200

    Delete unuseful ToDos

--- src/es/icarto/gvsig/catastro/evaluator/rules/CheckGeometryIsOverlapingAnotherOne.java
+++ src/es/icarto/gvsig/catastro/evaluator/rules/CheckGeometryIsOverlapingAnotherOne.java
@@ -45,7 +45,6 @@ public class CheckGeometryIsOverlapingAnotherOne implements IRule {
 		}
 	    }
 	} catch (ReadDriverException e) {
-	    // TODO Auto-generated catch block
 	    e.printStackTrace();
 	}
 	return checkRule;
